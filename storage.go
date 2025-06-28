3a132a6c056d92336ff7566ca3a746dbeeb44a75
Nasredinne <nasredineghella@gmail.com>
2025-06-28 03:09:26 +0100

    postgres 5

--- storage.go
+++ storage.go
@@ -33,14 +33,15 @@ func NewPostgresStore() (*PostgresStore, error) {
 	// 	return nil, err
 	// }
 
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-	)
+	// dsn := fmt.Sprintf(
+	// 	"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+	// 	os.Getenv("DB_HOST"),
+	// 	os.Getenv("DB_PORT"),
+	// 	os.Getenv("DB_USER"),
+	// 	os.Getenv("DB_PASSWORD"),
+	// 	os.Getenv("DB_NAME"),
+	// )
+	dsn := os.Getenv("DB_HOST")
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		log.Fatal(err)
